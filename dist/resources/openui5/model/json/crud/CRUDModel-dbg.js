38e6b21fc8d8b18e1bf5946f4d0d81535416b5c9
Mauricio Lauffer <mauriciolauffer@outlook.com>
2018-09-12 17:04:04 +1000

    0.0.4

--- dist/resources/openui5/model/json/crud/CRUDModel-dbg.js
+++ dist/resources/openui5/model/json/crud/CRUDModel-dbg.js
@@ -28,7 +28,7 @@ sap.ui.define([
    * @extends sap.ui.model.json.JSONModel
    *
    * @author Mauricio Lauffer
-   * @version 0.0.3
+   * @version 0.0.4
    *
    * @public
    * @alias openui5.model.json.crud.CRUDModel
