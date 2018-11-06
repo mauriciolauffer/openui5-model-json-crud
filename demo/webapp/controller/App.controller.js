ee97903c380eb61342c677bb0199b82259f09d81
Mauricio Lauffer <lauffer@outlook.com>
2018-11-07 10:34:48 +1100

    Updates demo

--- demo/webapp/controller/App.controller.js
+++ demo/webapp/controller/App.controller.js
@@ -1,20 +1,24 @@
 sap.ui.define([
+  'jquery.sap.global',
   'sap/ui/core/mvc/Controller',
   'openui5/model/json/crud/CRUDModel'
-], function(Controller, CRUDModel) {
+], function($, Controller, CRUDModel) {
   'use strict';
 
   return Controller.extend('mlauffer.demo.openui5.model.json.crud.controller.App', {
     onInit: function() {
-      const serviceUrl = 'https://api.jikan.moe/v3/';
-      this.getView().setModel(new CRUDModel(serviceUrl), 'CRUDModel');
-      this.getView().getModel('CRUDModel').read('search/anime?q=DBZ', '/DBZ')
+      const serviceUrl = 'https://api.jikan.moe/v3/'; //root backend URL
+      const crudModel = new CRUDModel(serviceUrl);
+      this.getView().setModel(crudModel, 'CRUDModel');
+      //'search/anime?q=DBZ' is the path to the service to be called, it'll be concatenated to serviceUrl
+      //'/DBZ' is the path to the property into the model, in case you want to updated the model with the response;
+      // empty value does not update the local model, it only returns the response
+      crudModel.read('search/anime?q=DBZ', '/DBZ')
         .then(function() {
-          console.log('Data selected from API');
+          $.sap.log.info('Data selected from API');
         })
         .catch(function(err) {
-          console.error(err);
-          console.error(err.toString());
+          $.sap.log.error(err.toString());
         });
     }
   });
