d934951c79cddcf34c1d9c26353428ae9624a676
Mauricio Lauffer <lauffer@outlook.com>
2019-03-26 13:09:54 +1100

    Refactoring demo

--- demo/webapp/controller/App.controller.js
+++ demo/webapp/controller/App.controller.js
@@ -1,19 +1,15 @@
 sap.ui.define([
   'sap/base/Log',
-  'sap/ui/core/mvc/Controller',
-  'openui5/model/json/crud/CRUDModel'
-], function(Log, Controller, CRUDModel) {
+  'sap/ui/core/mvc/Controller'
+], function(Log, Controller) {
   'use strict';
 
   return Controller.extend('mlauffer.demo.openui5.model.json.crud.controller.App', {
     onInit: function() {
-      const serviceUrl = 'https://api.jikan.moe/v3/'; //root backend URL
-      const crudModel = new CRUDModel(serviceUrl);
-      this.getView().setModel(crudModel, 'CRUDModel');
       //'search/anime?q=DBZ' is the path to the service to be called, it'll be concatenated to serviceUrl
       //'/DBZ' is the path to the property into the model, in case you want to updated the model with the response;
       // empty value does not update the local model, it only returns the response
-      crudModel.read('search/anime?q=DBZ', '/DBZ')
+      this.getView().getModel('CRUDModel').read('search/anime?q=DBZ', '/DBZ')
         .then(function() {
           Log.info('Data selected from API');
         })
