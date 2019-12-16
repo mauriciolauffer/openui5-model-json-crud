022de066e727778f9b780dba641c1e4d174a310a
Mauricio Lauffer <lauffer@outlook.com>
2019-12-17 09:30:39 +1100

    0.0.11

--- dist/resources/openui5/model/json/crud/library.js
+++ dist/resources/openui5/model/json/crud/library.js
@@ -3,4 +3,4 @@
  * (c) Copyright 2018-2019 Mauricio Lauffer
  * Licensed under the MIT license. See LICENSE file in the project root for full license information.
  */
-sap.ui.define([],function(){"use strict";return sap.ui.getCore().initLibrary({name:"openui5.model.json.crud",dependencies:["sap.ui.core"],controls:["openui5.model.json.crud.CRUDModel"],noLibraryCSS:true,version:"0.0.10"})});
+sap.ui.define([],function(){"use strict";return sap.ui.getCore().initLibrary({name:"openui5.model.json.crud",dependencies:["sap.ui.core"],controls:["openui5.model.json.crud.CRUDModel"],noLibraryCSS:true,version:"0.0.11"})});
