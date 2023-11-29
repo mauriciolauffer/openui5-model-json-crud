0a107ca177c01c7c55b379b717c4620f8b35f985
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-11-29 15:32:32 +1000

    test: wdio qunit service

---
+++ test/openui5/model/json/crud/unit/unit.test.js
@@ -0,0 +1,11 @@
+describe('QUnit test page', () => {
+  it('should pass QUnit tests', async () => {
+    const url = 'http://localhost:8080/test-resources/openui5/model/json/crud/unit/unitTests.html';
+    await browser.url(url);
+    const qunitResults = await browser.getQUnitResults();
+    expect(qunitResults).toBeTruthy();
+
+    const coverage = await browser.getCoverageReport();
+    expect(coverage?.statements.pct).toBeGreaterThan(80);
+  });
+});
